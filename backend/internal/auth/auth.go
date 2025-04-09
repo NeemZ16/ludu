c07a3c94d756f81902fd406575c0d36392182b7c
NeemZ16 <110858265+NeemZ16@users.noreply.github.com>
2025-04-09 19:28:09 -0400

    added devauth and cors

---
+++ backend/internal/auth/auth.go
@@ -0,0 +1,15 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"fmt"
+)
+
+func DevAuth(c *gin.Context) {
+	c.SetCookie("sessionToken", uuid.NewString(), 3600, "/", "localhost", false, true)
+	fmt.Println("DEV AUTH CALLED")
+
+	// send a plain text message in the body
+	c.String(200, "developer authentication successful!")
+}
